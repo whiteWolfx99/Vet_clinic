cfa81819df9f1fa4f54d250a22752577322ed5c3
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-01-20 15:57:50 +0300

    add query data and schema

--- schema.sql
+++ schema.sql
@@ -27,3 +27,30 @@ ALTER TABLE animals DROP COLUMN species;
 
 ALTER TABLE animals ADD COLUMN species_id INTEGER;
 ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+
+
+
+CREATE Table vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+  id SERIAL PRIMARY KEY,
+  species_id INTEGER NOT NULL,
+  vet_id INTEGER NOT NULL,
+  CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id),
+  CONSTRAINT fk_vet_id FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY,
+  animal_id INTEGER NOT NULL,
+  vet_id INTEGER NOT NULL,
+  date_of_visit DATE,
+  CONSTRAINT fk_animal_id FOREIGN KEY (animal_id) REFERENCES animals(id),
+  CONSTRAINT fk_vet_id FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
+
